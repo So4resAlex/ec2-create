988011292172e38a031f9e326deb7ddd87347142
Alex Soares <alex.soares@girotech.com.br>
2024-04-20 15:24:24 -0300

    feat: add validation from max value from ebs

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "ec2_server" {
   vpc_security_group_ids = var.vpc_security_group_ids
   root_block_device {
     delete_on_termination = true
-    volume_size = var.volume_size
+    volume_size = min(var.volume_size, 25)
     volume_type = "gp3"
   }
   tags = {
